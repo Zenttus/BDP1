d6dd0c93910dc4faa5e0fb205dc31ba240c5df78
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-08 16:27:20 -0400

    added initial mapreduce code

---
+++ src/main/java/edu/bigdata/mapreduce/FileReader.java
@@ -0,0 +1,48 @@
+package edu.bigdata.test;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.appender.SyslogAppender;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+
+public class FileReader {
+
+    public static void main(String[] args){
+
+        String fileName = "";
+        URI fileUri = URI.create(fileName);
+
+        Configuration conf = new Configuration();
+        conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+        conf.set("fs.file.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+
+        InputStream dataIn = null;
+
+        try {
+            FileSystem hdfs = FileSystem.get(fileUri, conf);
+            Path path = new Path(fileUri);
+            long bytesToRead = hdfs.getFileStatus(path).getLen();
+            System.out.println("Bytes to Read: " + bytesToRead);
+
+            dataIn = hdfs.open(path);
+            IOUtils.copyBytes(dataIn, System.out, bytesToRead, false);
+
+        }catch (Exception e){
+            System.out.println("boom a lo ziquitraque");
+            System.out.println(e);
+
+        } finally {
+            IOUtils.closeStream(dataIn);
+        }
+
+    }
+}
---
+++ src/main/java/edu/bigdata/mapreduce/TweetsMapper.java
@@ -0,0 +1,12 @@
+package edu.bigdata.test;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TweetsMapper extends Mapper<>{
+
+}
