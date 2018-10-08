4ebf8b950140026b4185568c13536deed25e7fe8
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-08 16:28:39 -0400

    new map reduce files

--- src/main/java/edu/bigdata/mapreduce/FileReader.java
+++ src/main/java/edu/bigdata/mapreduce/FileReader.java
@@ -1,15 +1,10 @@
-package edu.bigdata.test;
+package edu.bigdata.mapreduce;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.appender.SyslogAppender;
 
-import java.io.EOFException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 
--- src/main/java/edu/bigdata/mapreduce/TweetsMapper.java
+++ src/main/java/edu/bigdata/mapreduce/TweetsMapper.java
@@ -1,12 +1,17 @@
-package edu.bigdata.test;
-
-import java.io.IOException;
+package edu.bigdata.mapreduce;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class TweetsMapper extends Mapper<>{
+import java.io.IOException;
+
+public class TweetsMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    //TODO: format values
+    }
 
 }
