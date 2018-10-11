ccf9f4b047602ab248f83176e968311c28c21f6d
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-11 13:04:18 -0400

    Can read json

--- src/main/java/edu/bigdata/mapreduce/TweetsMapper.java
+++ src/main/java/edu/bigdata/mapreduce/TweetsMapper.java
@@ -5,16 +5,34 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+
+import org.codehaus.jettison.json.JSONException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.*;
+
 import java.io.IOException;
 
 public class TweetsMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        //TODO clean tweetData
-        //TODO word detector
+        String tweetText;
 
-        context.write(new Text(), new IntWritable(1));
+        JSONParser parser = new JSONParser();
+        String[] tuple = value.toString().split("\\n");
+        try {
+            for(int i=0; i<tuple.length; i++){
+                JSONObject obj = (JSONObject) parser.parse(tuple[i]);
+                tweetText = obj.get("text").toString();
+                context.write(new Text(tweetText), new IntWritable(1));
+            }
+        }catch(Exception e){
+            System.out.println("BOOM");
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 
 }
