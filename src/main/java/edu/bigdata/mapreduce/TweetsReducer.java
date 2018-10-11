5cf700f4e7959f4de94cb11fdabc54a38b4e3d97
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-10 21:38:01 -0400

    Implemented main class

--- src/main/java/edu/bigdata/mapreduce/TweetsReducer.java
+++ src/main/java/edu/bigdata/mapreduce/TweetsReducer.java
@@ -1,4 +1,15 @@
 package edu.bigdata.mapreduce;
 
-public class TweetsReducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class TweetsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        
+    }
 }
