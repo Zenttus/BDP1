4b9f6cb2e35201fb585e0996ad8475c95d60cbfd
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-17 11:11:15 -0400

    fixed correct main

--- src/main/java/mapreduce/getTotal/Main.java
+++ src/main/java/mapreduce/getTotal/Main.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        if(args.length != 3){
+        if(args.length != 2){
             System.err.println("Te faltan args(input path y output path).");
 
             System.exit(-1);
