b7acdb87e28ae8aff232e113cd706c18133d60da
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-17 13:16:12 -0400

    attempt fix on keywordMapper

--- src/main/java/mapreduce/keywords/KeywordMapper.java
+++ src/main/java/mapreduce/keywords/KeywordMapper.java
@@ -6,27 +6,46 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public class KeywordMapper extends Mapper<LongWritable, Text, Text, IntWritable>  {
 
-    private String keyWords = "[Tt]rump [Ff]lu [Zz]ika [Dd]iarrhea [Ee]bola [Hh]eadache [Mm]easles";
+    private String keyWords = "Trump trump Flu flu Zika zika Diarrhea diarrhea Ebola ebola Headache headache Measles measles";//TODO This can be done better with regex
+
+    private Text word = new Text();
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        Pattern pattern;
-        String cols[] = value.toString().split("\\s");
-        String word = cols[0];
-        String quantity = cols[1];
-        for(String w : keyWords.split("\\s")){
-             pattern = Pattern.compile(w);
-            if(pattern.matcher(value.toString()).find()){
-                context.write(new Text(word), new IntWritable(Integer.parseInt(quantity)) );
-                break;
-            }
-        }
+        boolean needToSkip = true;
 
+        JSONParser parser = new JSONParser();
+        String[] tuple = value.toString().split("\\n");
+        try {
+            for(int i=0; i<tuple.length; i++){
+                JSONObject obj = (JSONObject) parser.parse(tuple[i]);
+                StringTokenizer itr = new StringTokenizer(obj.get("text").toString());
+                while(itr.hasMoreTokens()){
+                    word.set(itr.nextToken());
+                    for(String stopWord : keyWords.split("\\s")){
+                        if(word.toString().contains(stopWord)) {
+                            needToSkip = false;
+                            break;
+                        }
+                    }
+                    if(!needToSkip) {
+                        context.write(word, new IntWritable(1));
+                    }
+                }
+            }
+        }catch(Exception e){
+            System.out.println("BOOM");
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 }
