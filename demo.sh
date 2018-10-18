bf85deb0a3010013b3e3f750c1d98c2aed457852
Pedro Santiago <pedro.santiago16@upr.edu>
2018-10-17 20:22:18 -0400

    changes script

--- demo.sh
+++ demo.sh
@@ -1,12 +1,12 @@
 echo "MAPREDUCE DEMO INITIATED...$1 $2"
 echo "Finding and counting words..."
-hadoop jar ./jars/words.jar mapreduce.words.Main $0 $1/words
-hadoop jar ./jars/getTotal.jar mapreduce.getTotal.Main $1/words/part-r-00001 $1/words/total
+hadoop jar ./jars/words.jar mapreduce.words.Main $1 $2/words
+hadoop jar ./jars/getTotal.jar mapreduce.getTotal.Main $2/words/part-r-00001 $2/words/total
 echo "DONE"
 echo "Finding key words..."
-hadoop jar ./jars/keyWords.jar mapreduce.keywords.Main $0 $1/keywords
+hadoop jar ./jars/keyWords.jar mapreduce.keywords.Main $1 $2/keywords
 echo "DONE"
 echo "Finding screen Names..."
-hadoop jar ./jars/screenNames.jar mapreduce.screenname.Main $0 $1/screenNames
-hadoop jar ./jars/getTotalKeys.jar mapreduce.getTotalKeys.Main $1/screenNames/part-r-00001 $1/screenNames/total
+hadoop jar ./jars/screenNames.jar mapreduce.screenname.Main $1 $2/screenNames
+hadoop jar ./jars/getTotalKeys.jar mapreduce.getTotalKeys.Main $2/screenNames/part-r-00001 $2/screenNames/total
 echo "FINISHED"
